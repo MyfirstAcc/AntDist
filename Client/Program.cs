2689bb7caa887f306a00969e148305e2c287326c
Denario <densuper2005@gmail.com>
2024-11-04 16:32:32 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class AntClient
+{
+    private TcpClient incomingClient;
+    private TcpClient outgoingClient;
+    private NetworkStream incomingStream;
+    private NetworkStream outgoingStream;
+    private string initData;
+    private int[] weights;
+    private int[] values;
+    private int weightLimit;
+    private double alpha;
+    private double beta;
+    private int nAnts;
+    public double[] pheromone;
+
+
+    public async Task ConnectToServer(IPAddress serverAddress, int incomingPort, int outgoingPort)
+    {
+        incomingClient = new TcpClient();
+        outgoingClient = new TcpClient();
+
+        // Подключение к входящему и исходящему порту на сервере
+        await incomingClient.ConnectAsync(serverAddress, incomingPort);
+        await outgoingClient.ConnectAsync(serverAddress, outgoingPort);
+
+        incomingStream = incomingClient.GetStream();
+        outgoingStream = outgoingClient.GetStream();
+
+        Console.WriteLine($"Подключен к серверу на обоих портах:({((IPEndPoint)incomingClient.Client.LocalEndPoint).Port})" +
+            $"--->({incomingPort}) ({((IPEndPoint)outgoingClient.Client.LocalEndPoint).Port})--->({outgoingPort})");
+    }
+
+    public async Task SendMessage(string message)
+    {
+        // Отправка сообщения на входящий порт сервера
+        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+        await outgoingStream.WriteAsync(messageBytes, 0, messageBytes.Length);
+
+        
+    }
+
+    public void SplitInitData(string initData)
+    {
+        var dataParts = initData.Split(';');
+        weights = Array.ConvertAll(dataParts[0].Split(','), int.Parse);
+        values = Array.ConvertAll(dataParts[1].Split(','), int.Parse);
+        weightLimit = int.Parse(dataParts[2]);
+        alpha = double.Parse(dataParts[3]);
+        beta = double.Parse(dataParts[4]);
+        nAnts = int.Parse(dataParts[5]);
+    }
+
+    public async Task<string> ReceivedMessage()
+    {
+        // Чтение ответа из потока исходящих сообщений
+        byte[] buffer = new byte[65000];
+        int bytesRead = await incomingStream.ReadAsync(buffer, 0, buffer.Length);
+        return Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    }
+
+
+    private (List<int> chosenItems, int currentValue) AntSolution()
+    {
+        double currentWeight = 0;    // Текущий вес рюкзака
+        int currentValue = 0;     // Текущая стоимость предметов в рюкзаке
+        var chosenItems = new List<int>();  // Список выбранных предметов
+        var availableItems = Enumerable.Range(0, weights.Length).ToList();  // Индексы доступных предметов
+
+        Random random = new Random();
+
+        // Процесс выбора предметов до тех пор, пока есть доступные предметы
+        while (availableItems.Count > 0)
+        {
+            var probabilities = availableItems.Select(i =>
+                Math.Pow(pheromone[i], alpha) * Math.Pow(values[i] / weights[i], beta)).ToArray();
+
+            // Нормализация вероятностей для получения распределения
+            double sumProb = probabilities.Sum();
+            var normalizedProb = probabilities.Select(p => p / sumProb).ToArray();
+
+            // Вычисление накопительных вероятностей для случайного выбора
+            var cumulativeProb = new double[normalizedProb.Length];
+            cumulativeProb[0] = normalizedProb[0];
+            for (int i = 1; i < normalizedProb.Length; i++)
+            {
+                cumulativeProb[i] = cumulativeProb[i - 1] + normalizedProb[i];
+            }
+
+            // Поиск случайного числа и выбор на основе накопительных вероятностей
+            double r = random.NextDouble();
+            int selectedItemIndex = Array.FindIndex(cumulativeProb, p => r <= p);
+            int selectedItem = availableItems[selectedItemIndex];
+
+            //Console.WriteLine($"Length of chosen_items: {chosenItems.Count}");
+
+            // Проверка возможности добавления выбранного предмета в рюкзак
+            if (currentWeight + weights[selectedItem] <= weightLimit)
+            {
+                // Добавление предмета в список выбранных
+                chosenItems.Add(selectedItem);
+                currentWeight += weights[selectedItem];
+                currentValue += values[selectedItem];
+                // Удаление выбранного предмета из списка доступных
+                availableItems.RemoveAt(selectedItemIndex);
+            }
+            else
+            {
+                // Завершение цикла, если больше нет подходящих предметов
+                break;
+            }
+
+        }
+
+        return (chosenItems, currentValue);
+}
+
+    public void Close()
+    {
+        incomingStream.Close();
+        outgoingStream.Close();
+        incomingClient.Close();
+        outgoingClient.Close();
+    }
+
+    static async Task Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("No arguments provided.");
+            return;
+        }
+        int nid = int.Parse(args[0]);
+        //Console.ReadLine();
+        AntClient client = new AntClient();
+        await client.ConnectToServer(IPAddress.Loopback, 7000 + nid, 6000 + nid);
+
+        await client.SendMessage("READY");
+
+        var str = await client.ReceivedMessage();
+        client.SplitInitData(str);
+
+        
+        while (true)
+        {
+            string inData = await client.ReceivedMessage();
+            if (inData == "end")
+            {
+                break;
+            }
+            else
+            {
+                client.pheromone = Array.ConvertAll(inData.Split(','), double.Parse);
+                int bestValue = 0;
+                int[] bestItems = [];
+                int[] allValues = new int[client.nAnts];
+                int[][] allItems = new int[client.nAnts][];
+
+
+                for (int i = 0; i < client.nAnts; i++)
+                {
+                    (List<int> chosenItems, int currentValue) = client.AntSolution();
+                    allValues[i] = currentValue;
+                    allItems[i] = chosenItems.ToArray();
+
+                    if (currentValue > bestValue)
+                    {
+                        bestValue = currentValue;
+                        bestItems = chosenItems.ToArray();
+                    }
+                }
+
+                string msg = "";
+                //Console.WriteLine(allItems.Length);
+
+                string allItemsStr = string.Join(",", Array.ConvertAll(allItems, items => string.Join(" ", items)));
+                string toSend = $"{bestValue};{string.Join(" ", bestItems)};{string.Join(" ", allValues)};{allItemsStr}";
+                //Console.WriteLine($"{ bestValue} {bestItems.Length} {allValues.Length} {allItems.Length}");
+                await client.SendMessage(toSend);
+            }
+           
+        }
+
+        client.Close();
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace AntColonyServer
+{
+    public class Server
+    {
+        private readonly string _path = @"C:\dev\csharp\AntDist\Client\bin\Debug\net8.0"; // Путь к Client.dll 
+        public int maxAnts = 20;
+        public int numClients = 1;
+        private double alpha = 1.0;
+        private double beta = 5.0;
+        private double RHO = 0.1;
+        private int countSubjects = 1000;
+        private int port = 2000;
+        private int bestValue = 0;
+        private int maxIter = 100;
+        private double[] pheromone;
+        private int Q = 100;
+        private List<TcpListener> incomingListeners = new List<TcpListener>();
+        private List<TcpListener> outgoingListeners = new List<TcpListener>();
+        private List<TcpClient> incomingClients = new List<TcpClient>();
+        private List<TcpClient> outgoingClients = new List<TcpClient>();
+        private int inPort;
+        private int outPort;
+        private IPAddress ipAddress;
+
+        public Server(IPAddress iPAddress, int inPort, int outPort)
+        {
+           this.inPort = inPort;
+           this.outPort = outPort;
+           this.ipAddress = iPAddress;
+           pheromone = Enumerable.Repeat(1.0, (int)countSubjects).ToArray();
+        }
+
+        private (int[] values, int[] weights, int weightLimit) GenerateModelParameters(int countSubjects)
+        {
+            //int[] weights = { 391, 444, 250, 330, 246, 400, 150, 266, 268, 293, 471, 388, 364, 493, 202, 161, 410, 270, 384, 486 };
+            //int[] values = { 55, 52, 59, 24, 52, 46, 45, 34, 34, 59, 59, 28, 57, 21, 47, 66, 64, 42, 22, 23 };
+            //int weightLimit = 500;
+
+            Random random = new Random(42); // Фиксируем начальное состояние
+
+            //// Генерация значений
+            int[] values = new int[countSubjects];
+            int[] weights = new int[countSubjects];
+            for (int i = 0; i < countSubjects; i++)
+            {
+                values[i] = 100 + random.Next(400); // Значения от 100 до 500
+                weights[i] = 10 + random.Next(90);  // Вес ant от 10 до 100
+            }
+
+            int weightLimit = 500; // Ограничение для веса
+
+            return (values, weights, weightLimit);
+        }
+
+
+        public async Task StartServer()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var (values, weights, weightLimit) = GenerateModelParameters(countSubjects);
+            var nAnts = CreateAndInitializeSockets(maxAnts, numClients);
+            stopwatch.Stop();
+            TimeSpan clientStartTimer = stopwatch.Elapsed;
+            Console.WriteLine($"--- Время запуска клиента: {clientStartTimer.TotalSeconds} с.");
+            await AcceptClients();
+           
+
+
+            for (int i = 0; i < outgoingClients.Count; i++)
+            {
+                var inSocket = incomingClients[i];
+                var incomingStream = inSocket.GetStream();
+                
+                byte[] buffer = new byte[1024];
+                int bytesRead = await incomingStream.ReadAsync(buffer, 0, buffer.Length);
+                if (bytesRead == 0) break;
+
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                if (message == "READY")
+                {
+                    var outSocket = outgoingClients[i];
+
+                    var outgoingStream = outSocket.GetStream();
+
+
+                    //Создание строки initData
+                    string initData = $"{string.Join(",", weights)};{string.Join(",", values)};{weightLimit};{alpha};{beta};{nAnts[i]}";
+
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(initData);
+                    await outgoingStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+
+                }
+            }
+
+            int bestValue = 0;
+            List<int> bestItems = new List<int>();
+            stopwatch.Reset();
+            stopwatch.Start();
+            for (int iter = 1; iter < maxIter; iter++)
+            {
+                var (tmpBestValue, tmpBestItems, allValues, allItems) = await OneStepAntColony();
+
+                if (tmpBestValue > bestValue)
+                {
+                    bestValue = tmpBestValue;
+                    bestItems = tmpBestItems;
+                }
+
+                // Обновление феромонов
+                for (int i = 0; i < pheromone.Length; i++)
+                {
+                    pheromone[i] = (1.0 - RHO) * pheromone[i];
+                }
+                for (int k = 0; k < maxAnts; k++)
+                {
+                    double sumValues = allValues.Sum(); // сумма элементов подмассива
+                    foreach (var itemIndex in allItems[k])
+                    {
+
+                        pheromone[itemIndex] += Q / sumValues;
+                    }
+                    
+                }
+                //Console.WriteLine($"Итерация {iter}: Стоимость {bestValue}");
+
+            }
+            foreach (var outSocket in outgoingClients)
+            {
+                 SendData(outSocket,"end");
+            }
+            stopwatch.Stop();
+            TimeSpan methodRunTimer = stopwatch.Elapsed;
+            Console.WriteLine($"--- Состав предметов: {string.Join(",", bestItems)}");
+            Console.WriteLine($"--- Общая стоимость : {bestValue}");
+            Console.WriteLine($"--- Время выполнения алгоритма: {methodRunTimer.TotalSeconds} с.");
+            Console.WriteLine($"--- Общее время выполнения: {(clientStartTimer+methodRunTimer).TotalSeconds} с.");
+
+        }
+
+
+        private async Task<(int bestValue, List<int> bestItems, List<int> allValues, List<int[]> allItems)> OneStepAntColony()
+        {
+            foreach (var outSocket in outgoingClients)
+            {
+                await SendData(outSocket, string.Join(",", pheromone));
+            }
+
+            // Инициализация переменных для хранения результатов
+            List<int> bestValues = new List<int>();
+            List<List<int>> bestItemsList = new List<List<int>>(numClients);
+            List<int> allValues = new List<int>();
+            List<int[]> allItems = new List<int[]>();
+
+            // Получение данных от каждого сокета
+            for (int i = 0; i < numClients; i++)
+            {
+                string response = await ReceiveData(incomingClients[i]);
+
+                var dataParts = response.Split(';');
+                allItems = ParseAntSelections(dataParts[3]);
+                allValues = dataParts[1].Split(' ').Select(int.Parse).ToList();
+                bestValues.Add(int.Parse(dataParts[0]));
+                bestItemsList.Add(dataParts[1].Split(' ').Select(int.Parse).ToList());
+                
+            }
+
+            // Выбор лучшего значения
+            int maxIndex = bestValues.IndexOf(bestValues.Max());
+            int bestValue = bestValues[maxIndex];
+            List<int> bestItems = bestItemsList[maxIndex];
+
+            return (bestValue, bestItems, allValues, allItems);
+        }
+
+        public List<int[]> ParseAntSelections(string input)
+        {
+            // Разделяем строку на подстроки по запятым 
+            // "1 2 3, 4 5, 6" -> ["1 2 3", "4 5", "6"]
+            string[] antSelections = input.Split(',')
+                                        .Select(s => s.Trim())
+                                        .ToArray();
+
+            // Преобразуем каждую подстроку в массив чисел 
+            // ["1 2 3", "4 5", "6"] -> [[1,2,3], [4,5], [6]]
+            List<int[]> result = antSelections
+                .Select(antSelection =>
+                    antSelection.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray())
+                .ToList();
+
+            return result;
+        }
+
+        public async Task AcceptClients()
+        {
+            for (int i = 0; i < incomingListeners.Count; i++)
+            {
+                // Принимаем входящее соединение
+                TcpClient incomingClient = await incomingListeners[i].AcceptTcpClientAsync();
+                incomingClients.Add(incomingClient);
+                Console.WriteLine($"Клиент {i} подключился к входящему порту {((IPEndPoint)incomingClient.Client.LocalEndPoint).Port}");
+
+                // Принимаем исходящее соединение
+                TcpClient outgoingClient = await outgoingListeners[i].AcceptTcpClientAsync();
+                outgoingClients.Add(outgoingClient);
+                Console.WriteLine($"Клиент {i} подключился к исходящему порту {((IPEndPoint)outgoingClient.Client.LocalEndPoint).Port}");
+            }
+        }
+
+        private async Task SendData(TcpClient outSocket, string message)
+        {
+            var outgoingStream = outSocket.GetStream();
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes(message);
+            await outgoingStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+        }
+
+
+        private async Task<string> ReceiveData(TcpClient inSocket)
+        {
+            var incomingStream = inSocket.GetStream();
+
+            byte[] buffer = new byte[65000];
+            int bytesRead = await incomingStream.ReadAsync(buffer, 0, buffer.Length);
+            if (bytesRead == 0) return "";
+            return Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+        }
+
+        private List<int> CreateAndInitializeSockets(int maxAnts, int numSock)
+        {
+            List<int> nAnts = new List<int>();
+
+            Console.WriteLine("% Вычисление количества муравьев для каждого сокета");
+
+            // Расчёт базового количества муравьев для каждого сокета
+            int baseNumAnt = maxAnts / numSock;
+            nAnts = Enumerable.Repeat(baseNumAnt, numSock).ToList();
+
+            // Распределение оставшихся муравьёв
+            int addAnt = maxAnts % numSock;
+            for (int i = 0; i < addAnt; i++)
+            {
+                nAnts[i]++;
+            }
+
+            Console.WriteLine("--- Количество муравьев на клиенте: ");
+            nAnts.ForEach(x => Console.WriteLine(x));
+
+            Console.WriteLine("% Создание и настройка сокетов");
+
+            for (int i = 0; i < numSock; i++)
+            {
+                inPort = inPort + i;
+                outPort = outPort + i;
+
+                // Настройка прослушивания входящих сообщений
+                TcpListener incomingListener = new TcpListener(this.ipAddress, inPort);
+                incomingListener.Start();
+                incomingListeners.Add(incomingListener);
+                Console.WriteLine($"Входящий слушатель запущен на порту {((IPEndPoint)incomingListener.LocalEndpoint).Port}");
+
+                // Настройка прослушивания исходящих сообщений
+                TcpListener outgoingListener = new TcpListener(this.ipAddress, outPort);
+                outgoingListener.Start();
+                outgoingListeners.Add(outgoingListener);
+                Console.WriteLine($"Исходящий слушатель запущен на порту {((IPEndPoint)outgoingListener.LocalEndpoint).Port}");
+
+                StartClientProcess(i);
+
+            }
+          
+            return nAnts;
+
+        }
+        private void StartClientProcess(int clientId)
+        {
+            Process clientProcess = new Process();
+            clientProcess.StartInfo.FileName = "dotnet";
+            clientProcess.StartInfo.Arguments = $"Client.dll {clientId}";
+            clientProcess.StartInfo.WorkingDirectory = _path;
+            //clientProcess.StartInfo.UseShellExecute = false; // Если требуется, укажите false
+            //clientProcess.StartInfo.RedirectStandardOutput = true; // Если хотите видеть вывод
+            //clientProcess.StartInfo.RedirectStandardError = true; // Для ошибок
+            clientProcess.Start();
+        }
+    }
+
+
+        class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Server server = new Server(IPAddress.Loopback,6000, 7000);
+            //int result;
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        result = ReadInt32("Введите кол-во муравьев: ");
+            //        break;
+            //    }
+            //    catch (FormatException ex)
+            //    {
+            //        Console.WriteLine($"Некорректный ввод: {ex.Message}");
+            //    }
+            //}
+            //server.maxAnts = result;
+
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        result = ReadInt32("Введите кол-во клиентов: ");
+            //        break;
+            //    }
+            //    catch (FormatException ex)
+            //    {
+            //        Console.WriteLine($"Некорректный ввод: {ex.Message}");
+            //    }
+            //}
+            //server.numClients = result;
+
+            await server.StartServer();
+            
+            Console.ReadLine();
+        }
+
+        static int ReadInt32(string prompt)
+        {
+            Console.Write(prompt);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+
+}
